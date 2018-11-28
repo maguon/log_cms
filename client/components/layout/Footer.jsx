3a4b7700e6c9541b9a475843c8f877e384a6ee3c
star920626 <jing.zhang@myxxjs.com>
2018-11-28 13:44:29 +0800

    前端 footer  样式  假数据

--- client/components/layout/Footer.jsx
+++ client/components/layout/Footer.jsx
@@ -10,19 +10,58 @@ class Footer extends React.Component {
 
         return (
             <div className="page-footer">
-                <div className="footer-copyright">
-                    <div className="container">
-                        <span><a className="grey-text text-lighten-4" href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT</a> All rights reserved.</span>
-                        <span className="right hide-on-small-only"> Design and Developed by <a className="grey-text text-lighten-4" href="https://pixinvent.com/">PIXINVENT</a></span>
-
-                        <ul className="tree_1 col s12" id="menu2">
-
-                        </ul>
-                        <div className="footer">大连顺通圣世物流有限公司&nbsp;备案号:辽ICP备14011916号</div>
+                <div className="foot">
+                    <div className="footerLi row col s12">
+                        <div className="span3 col">
+                            <div className="lined">
+                                <h2><a href="">公司简介</a></h2>
+                                <h5>Company profile</h5>
+                                <span className="bolded-line"></span>
+                            </div>
+                            <p>
+                                &nbsp;&nbsp;大连顺通圣世物流有限公司成立于2013年1月14日，位于大连市保税区瑞港路，临近疏港高速，主营商品汽车运输、普通货物运输、集装箱运输、零部件运输、仓储等，并在沈阳、长春、天津、上海、广州、成都设有办事处。
+                            </p>
+                        </div>
+                        <div className="span3 col">
+                            <div className="lined">
+                                <h2><a href="frontrecruitment?p=1&amp;md=6">人才招聘</a></h2>
+                                <h5>personnel recruitment</h5>
+                                <span className="bolded-line"></span>
+                            </div>
+                            <div>
+                            </div>
+                         </div>
+                        <div className="span3 col">
+                            <div className="lined">
+                                <h2>站内查询</h2>
+                                <h5>Find</h5>
+                                <span className="bolded-line"></span>
+                            </div>
+                            <div className="input-append row  col s12">
+                                <input type="hidden" name="p" id="searchpage" value="1" />
+                                <input className="search-width col s9" id="appendedInputButton"  name="searchNews.newsName" type="text"/>
+                                 <button className="btn btn-theme col s3" type="submit">Go</button>
+                            </div>
+                        </div>
+                        <div className="span3 col">
+                            <div className="lined">
+                                <h2><a href="frontnews?md=7">联系我们</a></h2>
+                                <h5>CONTACT US</h5>
+                                <span className="bolded-line"> </span>
+                            </div>
+                            <div className="f_end">
+                                <p>地址：辽宁省大连市保税区瑞港路6号（奇瑞工厂2号门对面）
+                                    <a href="frontnews?md=7">地图→</a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                 </div>
+
+
+                <div className="footer">大连顺通圣世物流有限公司&nbsp;备案号:辽ICP备14011916号</div>
             </div>
-        );
-    }
-}
-export default (Footer)
+            );
+        }
+     }
+    export default (Footer)
--- client/components/layout/Header.jsx
+++ client/components/layout/Header.jsx
@@ -1,3 +1,6 @@
+
+
+
 'use strict'
 const React = require('react')
 
@@ -11,24 +14,22 @@ class Header extends React.Component {
         return (
             <div className="nav-wrapper">
                 <div className="row">
-                    <div className="col s12 m10 offset-m1">
+                    <div className="col s12 m4 l3" id="nav_bg">
 
-                        <div id="nav_bg" className="col s12">
-                            <div className="logoimg col s5">
-                                <a href="" className="col s12">
-                                    <img alt="logo" src="/backend/assets/images/logo.png"  id="logo_img"/>
-                                </a>
-                            </div>
-
-                            <div className="nav_ce col s7">
-                                <ul className="tree_1 col s12" id="menu2">
-                                    <li className='nav_block col s1'><a className="collection-item white-text" href='menu'>首页 </a></li>
-                                     {
-                                        this.props.menuList.map((menuItem,index) => <li className='nav_block col s1'><a className="collection-item white-text"  key={index} href={"/view/menu/"+menuItem._id.toString()+"/news"}>{menuItem.menu_name}</a></li>)
-                                    }
-                                </ul>
-                            </div>
+                        <div className="logoimg col s4 offset-s1">
+                            <a href={"/view/menu/"} className="col s12">
+                                <img alt="logo" src="/backend/assets/images/logo.png"  id="logo_img"/>
+                            </a>
                         </div>
+
+                        <div className="nav_ce col s7">
+                            <ul className="tree_1 col s12">
+                                {
+                                    this.props.menuList.map((menuItem,index) => <li className='nav_block col'><a className="collection-item white-text"  key={index} href={"/view/menu/"+menuItem._id.toString()+"/news"}>{menuItem.menu_name}</a></li>)
+                                }
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </div>
