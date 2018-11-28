3f7dc3ac4de3c109776a595bbcb4358b4e377eaf
star920626 <jing.zhang@myxxjs.com>
2018-11-28 14:29:55 +0800

    前端 list  样式

--- client/components/ListComponent.jsx
+++ client/components/ListComponent.jsx
@@ -14,6 +14,23 @@ export default class ListComponent  extends React.Component {
                 <Header menuList={this.props.menuList}/>
                 <div className="rows">
                     <div className="collection">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         {
                             this.props.newsList.map((newsItem,index) => <a className="collection-item" key={index} href={"/view/news/"+newsItem._id.toString()}>{newsItem.news_content}</a>)
                         }</div>
