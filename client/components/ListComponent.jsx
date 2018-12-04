f98d569cc3d446ac429312db23d87315855eae80
star920626 <jing.zhang@myxxjs.com>
2018-12-04 14:55:34 +0800

    前端分页 样式

--- client/components/ListComponent.jsx
+++ client/components/ListComponent.jsx
@@ -10,9 +10,75 @@ export default class ListComponent  extends React.Component {
         super(props);
     }
 
-    render () {
-        return (
 
+    render () {
+        let prePage;
+        if(this.props.currentPage==='1'){
+            prePage =(
+                <div className="fenye">
+
+                    <span className="fenyez12g fenyez1 pageList">共{
+                        this.props.pageObj.totalCount
+                    }条
+                    </span>
+                    {
+                        <a   className="pageList" style={{marginLeft:10+'px'}}  >上一页</a>
+                    }
+
+                    <span className="fenyez12g pageList fenyez2 center" style={{marginLeft:10+'px'}}> {this.props.currentPage}</span>
+
+                    {
+                        <a className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"}  >下一页</a>
+                    }
+                </div>
+            )
+
+        }
+        else  if(this.props.currentPage!=='1'&&this.props.currentPage!== ""+this.props.pageObj.totalPage){
+            prePage =(
+                <div className="fenye">
+
+                    <span className="fenyez12g fenyez1 pageList">共{
+                        this.props.pageObj.totalCount
+                    }条
+                    </span>
+                    {
+                        <a  className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"}   >上一页</a>
+                    }
+
+                    <span className="fenyez12g pageList fenyez2 center" style={{marginLeft:10+'px'}}> {this.props.currentPage}</span>
+
+                    {
+                        <a className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"}  >下一页</a>
+                    }
+                </div>
+            )
+        }
+        else if(this.props.currentPage=== ""+this.props.pageObj.totalPage){
+            prePage =(
+
+                <div className="fenye">
+
+                        <span className="fenyez12g fenyez1 pageList">共{
+                            this.props.pageObj.totalCount
+                        }条
+                        </span>
+                    {
+                        <a  className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"}   >上一页</a>
+                    }
+
+                    <span className="fenyez12g pageList fenyez2 center" style={{marginLeft:10+'px'}}> {this.props.currentPage}</span>
+
+                    {
+                        <a className="pageList" style={{marginLeft:10+'px'}}  >下一页</a>
+                    }
+                </div>
+            )
+        }
+        else {
+            return;
+        }
+        return (
             <div>
                 <Header menuList={this.props.menuList}/>
                 <div className="rows">
@@ -24,8 +90,8 @@ export default class ListComponent  extends React.Component {
                             </div>
                             <div className="about_word">
                                 <div className="kect">
-                                    <div className="mores"><a href={"/view/index"} className="black-text" >首页</a>&nbsp;&gt;&nbsp;
-                                        <a className="black-text"  href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/"+this.props.newsList[0].menu_id.menu_type+"/page/1/news"}>{this.props.menuName}</a></div>
+                                    <div className="mores"><a href="index" className="black-text">首页</a>&nbsp;&gt;&nbsp;
+                                        <a href="#" className="black-text" >{this.props.menuName}</a></div>
                                 </div>
 
                                 <div className="ab_neir">
@@ -41,31 +107,19 @@ export default class ListComponent  extends React.Component {
 
                                     </ul>
 
-                                    <div className="fenye">
-
-                                         <span className="fenyez12g fenyez1 pageList">共{
-                                             this.props.pageObj.totalCount
-                                         }条
-                                        </span>
-
-                                        {
-                                            <a id="page" className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"}   >上一页</a>
-                                        }
-
-                                        <span className="fenyez12g pageList fenyez2 center" style={{marginLeft:10+'px'}}> {this.props.currentPage}</span>
-
-                                        {
-                                            <a className="pageList" style={{marginLeft:10+'px'}} href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"}  >下一页</a>
-                                        }
-
-                                    </div>
+                                    {prePage}
                                 </div>
+
+
+
+
+
                             </div>
                         </div>
 
+                    </div>
+                    <Footer contactList={this.props.contactList} recruitList={this.props.recruitList} profileList={this.props.profileList} />
                 </div>
-                <Footer contactList={this.props.contactList} recruitList={this.props.recruitList} profileList={this.props.profileList} />
-            </div>
             </div>
         );
     }
