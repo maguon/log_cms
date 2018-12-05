d2e11e0bca8e288951a47ab9507de284187e176d
zwl123 <wenlong.zhang@myxxjs.com>
2018-12-05 16:12:53 +0800

    更新图片链接

--- client/components/NewsDetailsComponent.jsx
+++ client/components/NewsDetailsComponent.jsx
@@ -69,7 +69,7 @@ export default class NewsDetailsComponent  extends React.Component {
                             <div className="about_word">
                                 <div className="kect">
                                     <div className="mores"><a href={"/view/index"} className="black-text" >首页</a>&nbsp;&gt;&nbsp;
-                                        <a className="black-text"  href="">{this.props.menuName}</a></div>
+                                        <a className="black-text"  href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/"+this.props.newsList[0].menu_id.menu_type+"/page/1/news"}>{this.props.menuName}</a></div>
                                 </div>
 
                                 <div className="ab_neir">
