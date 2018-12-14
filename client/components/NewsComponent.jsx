5424801406b03944b31dfc2c24d83a5614ddbe38
zwl123 <wenlong.zhang@myxxjs.com>
2018-12-14 10:39:59 +0800

    新闻栏目增加二级菜单

--- client/components/NewsComponent.jsx
+++ client/components/NewsComponent.jsx
@@ -9,6 +9,18 @@ export default class NewsComponent  extends React.Component {
         super(props);
     }
     render () {
+        let twoMenuName;
+        if(this.props.newsList.length===0){
+            twoMenuName = "";
+        }else if (this.props.newsList[0].menu_id.menu_pid !== '-1') {
+            twoMenuName = (
+                <a className="black-text"
+                   href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/" + this.props.newsList[0].menu_id.menu_type + "/page/1/news"}>&nbsp;&gt;&nbsp;{this.props.newsList[0].menu_id.menu_name}</a>
+            )
+        }else{
+            twoMenuName = "";
+        }
+
         return (
             <div>
                 <Header menuList={this.props.menuList}/>
@@ -17,11 +29,21 @@ export default class NewsComponent  extends React.Component {
                         <div className="wrap1003">
                             <div className="ab_huod">
                                 <div className="ab_huod_tit">{this.props.menu[0].menu_name}</div>
+                                <ul className="ab_huod_fenlei" id="treeDemo">
+                                    {
+                                        this.props.twoMenuNameList.map((twoMenuNameItem,index) =>
+                                            <li className="level0 treeDemo">
+                                                <a className="level0" key={index} href={"/view/menu/"+twoMenuNameItem._id+"/menuType/"+twoMenuNameItem.menu_type+"/page/1/news"}>{twoMenuNameItem.menu_name}</a>
+                                            </li>)
+                                    }
+                                </ul>
                             </div>
                             <div className="about_word">
                                 <div className="kect">
                                     <div className="mores"><a href={"/view/index"} className="black-text" >首页</a>&nbsp;&gt;&nbsp;
-                                        <a className="black-text"  href="">{this.props.menu[0].menu_name}</a></div>
+                                        <a className="black-text" href={"/view/menu/"+this.props.menu[0]._id+"/menuType/"+this.props.menu[0].menu_type+"/page/1/news"}>{this.props.menu[0].menu_name}</a>
+                                        {twoMenuName}
+                                    </div>
                                 </div>
 
                                 <div className="ab_neir">
