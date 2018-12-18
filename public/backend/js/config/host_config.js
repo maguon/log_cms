63332e4b6249e41858b1e73b055bf2625d78888a
star920626 <jing.zhang@myxxjs.com>
2018-12-18 16:20:00 +0800

    修改格式

--- public/backend/js/config/host_config.js
+++ public/backend/js/config/host_config.js
@@ -4,12 +4,7 @@
 var hostService = angular.module("hostService", []);
 hostService.factory("$host", function () {
     var _this = {
-        api_url: "http://192.168.191.143:8100/api",
-      /*  api_url: "http://localhost:8100/api",*/
-        //file_url: "http://stg.myxxjs.com:9002/api",
-        //record_url: "http://stg.myxxjs.com:9004/api",
-        //socket_url: "ws://stg.myxxjs.com:9005"
-
+        api_url: "http://192.168.191.143:8100/api"
     };
     return _this;
 });
