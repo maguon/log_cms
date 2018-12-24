348fb77937f11885d8b26512ffda7d25eef11649
star920626 <jing.zhang@myxxjs.com>
2018-12-24 10:36:44 +0800

    修改图片上传

--- client/components/layout/Footer.jsx
+++ client/components/layout/Footer.jsx
@@ -1,24 +1,11 @@
-
-
 'use strict'
 const React = require('react');
 
 class Footer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value:'' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event){
-        this.setState({value:event.target.value});
-        console.log(this.state.value)
-    }
-    handleSubmit(event){
-        alert(this.state.value);
-        event.preventDefault();
-    }
 
+    }
     render() {
         let recruitLink;
         let contactLink;
