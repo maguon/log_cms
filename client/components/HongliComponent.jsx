3273f63be0d9b8ba2fe7a0452be31cdbd23e007e
zwl123 <wenlong.zhang@myxxjs.com>
2019-01-18 09:25:31 +0800

    新增洪溧页面

---
+++ client/components/HongliComponent.jsx
@@ -0,0 +1,92 @@
+'use strict'
+
+import React from 'react';
+
+import  Header from './layout/Header';
+import  Footer from './layout/Footer';
+
+export default class HongliComponent  extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        let carLink;
+        if(this.props.newsImageList.length===0){
+            carLink = "";
+        }else{
+            carLink = "/view/menu/"+this.props.newsImageList[0].menu_id._id+"/menuType/"+this.props.newsImageList[0].menu_id.menu_type+"/page/1/news";
+        }
+        return (
+            <div>
+                <Header menuList={this.props.menuList}/>
+                <div className="rows">
+                    <div className="collection">
+                        <div className="example">
+                            <div className="ft-carousel" id="carousel_1">
+                                <ul className="carousel-inner">
+                                    {
+                                        this.props.newsList.map((newsItem,index) =>
+                                            <li className="carousel-item">
+                                                <a className="collection-item" style={{padding: 0 + 'em'}} key={index} href={"/view/menu/"+newsItem.menu_id._id+"/menuType/"+newsItem.menu_id.menu_type+"/news/"+newsItem._id.toString()+"/page/1"}>
+                                                    <img src={"/api/image/"+newsItem.news_image} alt={newsItem.news_title} className="hdp" />
+                                                    <span className="yx-rotation-title"> </span>
+                                                    <span className="yx-rotation-t">{newsItem.news_title}</span>
+                                                </a>
+
+                                            </li>
+                                        )
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+
+
+
+                        <div id="companyInter">
+                            <div>
+                                <div className="ff">
+                                    <div className="companyH2">
+                                        <h2>
+                                            <span>公司简介</span>
+                                        </h2>
+                                        <h5>Companyprofile</h5>
+                                        <span className="span1"></span>
+                                    </div>
+                                    <p className='Pspan'>大连顺通圣世物流有限公司成立于2013年1月14日，位于大连市保税区瑞港路，临近疏港高速，主营商品汽车运输、普通货物运输、集装箱运输、零部件运输、仓储等，并在沈阳、长春、天津、上海、广州、成都设有办事处。</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div  id='showCarPlace'>
+                            <div id="hdjy">
+                                <div className="kect_index">
+                                    <h2 className="ab_guanyu_index">
+                                        <span><a href={carLink}>车辆场地展示</a></span>
+                                    </h2>
+                                    <h5>Car show</h5>
+                                </div>
+
+                                <div className="showImg">
+                                    {
+                                        this.props.newsImageList.map((imageItem,index) =>
+                                            <div className="silhouette_block">
+                                                <div className="silhouette_block_pic">
+                                                    <a className="collection-item" key={index} href={"/view/menu/"+imageItem.menu_id._id+"/menuType/"+imageItem.menu_id.menu_type+"/picture/"+imageItem._id.toString()+"/page/1"}>
+                                                        <img className='imgShow' src={"/api/image/"+imageItem.news_image} /></a> </div>
+                                                <div className="silhouette_block_word">   {imageItem.news_title} </div>
+
+                                            </div>)
+                                    }
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer profileList={this.props.profileList} recruitList={this.props.recruitList} contactList={this.props.contactList} />
+            </div>
+        );
+    }
+
+}
--- server/controller/view/MenuViewController.js
+++ server/controller/view/MenuViewController.js
@@ -7,6 +7,7 @@ const {StyleModel} = require('../../modules/schemas');
 const {MenuModel} = require('../../modules/schemas');
 const {NewsModel} = require('../../modules/schemas');
 import MenuComponent from '../../../client/components/MenuComponent';
+import HongliComponent from '../../../client/components/HongliComponent';
 
 const getMenuView = (req, res, next) => {
     let newsObj = {};
@@ -123,7 +124,123 @@ const getMenuView = (req, res, next) => {
 
 }
 
+const getHongliView = (req, res, next) => {
+    let newsObj = {};
+    let title = "";
+    let cssArray=[];
+    let scriptArray=[];
+    new Promise((resolve) => {
+        let query = StyleModel.find({});
+        query.exec((error,rows)=> {
+            if(error){
+                resUtil.resetErrorPage(res,error);
+            }else{
+                title = rows[0].title;
+                for(let i = 0;i<rows[0].css_link.length;i++){
+                    cssArray[i] = rows[0].css_link[i];
+                }
+                for(let i = 0;i<rows[0].js_link.length;i++){
+                    scriptArray[i] = rows[0].js_link[i];
+                }
+                console.log(title);
+                console.log(cssArray);
+                console.log(scriptArray);
+                resolve();
+            }
+        });
+
+    }).then(() => {
+        new Promise((resolve) => {
+            let query = MenuModel.find({});
+            query.where('menu_pid').equals('-1');
+            query.where('menu_status').equals('1');
+            query.sort('menu_num').exec((error,rows)=> {
+                if(error){
+                    resUtil.resetErrorPage(res,error);
+                }else{
+                    resolve(rows);
+                }
+            });
+        }).then((menuList) => {
+            new Promise((resolve) => {
+                let query = NewsModel.find({}).populate('menu_id');
+                query.where('menu_id').equals('5bfbb72506e91f3814c8d0ec');
+                query.where('news_status').equals('1');
+                query.skip(parseInt('0')).limit(parseInt('8'));
+                query.sort('news_num').exec((error,rows)=> {
+                    if(error){
+                        resUtil.resetErrorPage(res,error);
+                    }else{
+                        newsObj.newsImageList = rows;
+                        resolve(menuList);
+                    }
+                })
+            }).then((menuList) => {
+                new Promise((resolve) => {//删除合作伙伴
+                    let query = NewsModel.find({}).populate('menu_id');
+                    query.where('menu_id').equals('5bfbb65606e91f3814c8d0ea');
+                    query.where('news_status').equals('1');
+                    query.skip(parseInt('0')).limit(parseInt('10'));
+                    query.sort('news_num').exec((error,rows)=> {
+                        if(error){
+                            resUtil.resetErrorPage(res,error);
+                        }else{
+                            newsObj.partnersList = rows;
+                            resolve(menuList);
+                        }
+                    })
+                }).then((menuList) => {
+                    new Promise((resolve) => {
+                        let query = NewsModel.find({}).populate('menu_id').populate('menu_id');
+                        query.where('menu_id').equals('5c00a754a0c6192580565b26');
+                        query.where('news_status').equals('1');
+                        query.skip(parseInt('0')).limit(parseInt('5'));
+                        query.sort('news_num').exec((error,rows)=> {
+                            if(error){
+                                resUtil.resetErrorPage(res,error);
+                            }else{
+                                newsObj.recruitList = rows;
+                                resolve(menuList);
+                            }
+                        })
+                    }).then((menuList) => {
+                        new Promise((resolve) => {
+                            let query = NewsModel.find({}).populate('menu_id').populate('menu_id');
+                            query.where('menu_id').equals('5bfbb62c06e91f3814c8d0e8');
+                            query.where('news_status').equals('1');
+                            query.sort('news_num').exec((error,rows)=> {
+                                if(error){
+                                    resUtil.resetErrorPage(res,error);
+                                }else{
+                                    newsObj.contactList = rows;
+                                    resolve(menuList);
+                                }
+                            })
+                        }).then((menuList) => {
+                            let query = NewsModel.find({}).populate('menu_id');
+                            query.where('roll_flag').equals('1');
+                            query.where('news_status').equals('1');
+                            query.skip(parseInt('0')).limit(parseInt('4'));
+                            query.sort('news_num').exec((error,rows)=> {
+                                if(error){
+                                    resUtil.resetErrorPage(res,error);
+                                }else{
+                                    const componentString = ReactDOMServer.renderToString(
+                                        <HongliComponent {... {menuList:menuList,newsList:rows,newsImageList:newsObj.newsImageList,partnersList:newsObj.partnersList,contactList:newsObj.contactList,profileList:newsObj.profileList,recruitList:newsObj.recruitList}}/>);
+                                    resUtil.resetMainPage(res,title,cssArray,scriptArray,componentString)
+                                }
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+
+}
+
 
 module.exports = {
-    getMenuView
+    getMenuView,
+    getHongliView
 }
