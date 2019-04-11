416f730f4c358ae8601d12b8845844fa24ff1329
star920626 <jing.zhang@myxxjs.com>
2019-04-11 08:38:48 +0800

    添加洺源科技修改图片样式

--- client/components/MingyuanComponent.jsx
+++ client/components/MingyuanComponent.jsx
@@ -84,7 +84,6 @@ export default class MingyuanComponent  extends React.Component {
                                         <div className='col s4 productPart'>
                                             <div className="productPartPic">
                                                 <div className="silhouette_block_pic">
-
                                                     <a className="collection-item" href="javascript:void(0)">
                                                         <img className='imgShow' src={"/api/image/" + newsItem.news_image}/>
                                                     </a>
