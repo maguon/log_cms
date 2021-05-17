ea479343c674081d424226208a6a40c2b7f89ca7
star920626 <jing.zhang@myxxjs.com>
2021-05-17 16:44:27 +0800

    格式

--- client/components/ListComponent.jsx
+++ client/components/ListComponent.jsx
@@ -2,62 +2,59 @@
 
 import React from 'react';
 
-import  Header from './layout/Header';
-import  Footer from './layout/Footer';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 
-var sectionStyle ;
+var sectionStyle;
 
-export default class ListComponent  extends React.Component {
+export default class ListComponent extends React.Component {
     constructor(props) {
         super(props);
     }
 
 
-    render () {
-
+    render() {
         let twoMenuName;
-        if(this.props.twoMenuNameList.length==0){twoMenuName=''}
-        else if (this.props.newsList[0].menu_id !== '-1') {
+        if (this.props.twoMenuNameList.length == 0) {
+            twoMenuName = ''
+        } else if (this.props.newsList[0].menu_id !== '-1') {
             twoMenuName = (
-            <a className="black-text"
-               href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/" + this.props.newsList[0].menu_id.menu_type + "/page/1/news"}>&nbsp;&gt;&nbsp;{this.props.newsList[0].menu_id.menu_name}</a>
-            )}
-        else{twoMenuName=''}
-        if(this.props.newsList[0].menu_id.banner_image!==''&&this.props.newsList[0].menu_id.banner_image!==null){
-            sectionStyle={
-                backgroundImage: `url(`+(this.props.newsList[0].menu_id.banner_image)+`)`
-            };
+                <a className="black-text"
+                   href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/" + this.props.newsList[0].menu_id.menu_type + "/page/1/news"}>&nbsp;&gt;&nbsp;{this.props.newsList[0].menu_id.menu_name}</a>
+            )
+        } else {
+            twoMenuName = ''
         }
-        else {
-            if(this.props.menu[0].banner_image!==''&&this.props.menu[0].banner_image!==null){
-                sectionStyle={
-                    backgroundImage: `url(`+(this.props.menu[0].banner_image)+`)`
+        if (this.props.newsList[0] && this.props.newsList[0].menu_id && this.props.newsList[0].menu_id.banner_image && this.props.newsList[0].menu_id.banner_image != '') {
+            sectionStyle = {
+                backgroundImage: `url(` + (this.props.newsList[0].menu_id.banner_image) + `)`
+            };
+        } else {
+            if (this.props.menu[0].banner_image != '' && this.props.menu[0].banner_image != null) {
+                sectionStyle = {
+                    backgroundImage: `url(` + (this.props.menu[0].banner_image) + `)`
                 };
-            }
-            else {
-                sectionStyle={
+            } else {
+                sectionStyle = {
                     backgroundImage: `url(/uploads/banner.png)`
                 };
             }
         }
-
-
-
         let prePage;
         let items = [];
         let pageList;
         for (var i = 1; i <= this.props.pageObj.totalPage; i++) {
-            if(this.props.pageObj.totalPage!==0&&this.props.currentPage=='1'&&this.props.currentPage!==  ""+this.props.pageObj.totalPage){
-                if(this.props.currentPage==i){
-                    items.push( <li className="active"><a href="#">{i}</a></li>);
-                }
-                else {
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"} >{i}</a></li>);
+            if (this.props.pageObj.totalPage !== 0 && this.props.currentPage == '1' && this.props.currentPage !== "" + this.props.pageObj.totalPage) {
+                if (this.props.currentPage == i) {
+                    items.push(<li className="active"><a href="#">{i}</a></li>);
+                } else {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/news"}>{i}</a>
+                    </li>);
                 }
 
 
-                prePage =(
+                prePage = (
                     <div className="fenye">
                         <div className="text-center">
                             <ul className="pagination">
@@ -82,21 +79,20 @@ export default class ListComponent  extends React.Component {
                     </div>
                 )
 
-            }
-            else if(this.props.pageObj.totalPage!==0&&this.props.currentPage!=='1'&&this.props.currentPage!== ""+this.props.pageObj.totalPage){
-                if(this.props.currentPage==i){
-                    items.push( <li className="active"><a href="#">{i}</a></li>);
-                }
-                else if(this.props.currentPage<i){
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"} >{i}</a></li>);
-                }
-                else{
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"} >{i}</a></li>);
+            } else if (this.props.pageObj.totalPage !== 0 && this.props.currentPage !== '1' && this.props.currentPage !== "" + this.props.pageObj.totalPage) {
+                if (this.props.currentPage == i) {
+                    items.push(<li className="active"><a href="#">{i}</a></li>);
+                } else if (this.props.currentPage < i) {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/news"}>{i}</a>
+                    </li>);
+                } else {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) - 1) + "/news"}>{i}</a>
+                    </li>);
 
                 }
-                prePage =(
+                prePage = (
                     <div className="fenye">
                         <div className="text-center">
                             <ul className="pagination">
@@ -119,22 +115,21 @@ export default class ListComponent  extends React.Component {
                     }*/}
                     </div>
                 )
-            }
-            else if(this.props.pageObj.totalPage!==0&&this.props.currentPage== ""+this.props.pageObj.totalPage&&this.props.currentPage!=='1'){
-                if(this.props.currentPage==i){
-                    items.push( <li className="active"><a href="#">{i}</a></li>);
-                }
-                else if(this.props.currentPage<i){
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"} >{i}</a></li>);
-                }
-                else{
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"} >{i}</a></li>);
+            } else if (this.props.pageObj.totalPage != 0 && this.props.currentPage == "" + this.props.pageObj.totalPage && this.props.currentPage != '1') {
+                if (this.props.currentPage == i) {
+                    items.push(<li className="active"><a href="#">{i}</a></li>);
+                } else if (this.props.currentPage < i) {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/news"}>{i}</a>
+                    </li>);
+                } else {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) - 1) + "/news"}>{i}</a>
+                    </li>);
 
                 }
 
-                prePage =(
+                prePage = (
 
                     <div className="fenye">
                         <div className="text-center">
@@ -160,23 +155,20 @@ export default class ListComponent  extends React.Component {
                     }*/}
                     </div>
                 )
-            }
-            else {
-                if(this.props.currentPage==i){
-                    items.push( <li className="active"><a href="#">{i}</a></li>);
-                }
-                else if(this.props.currentPage<i){
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/news"} >{i}</a></li>);
-                }
-                else{
-                    items.push( <li><a style={{marginLeft:10*{i}+'px'}}
-                                       href={"/view/menu/"+this.props.menu[0]._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/news"} >{i}</a></li>);
+            } else {
+                if (this.props.currentPage == i) {
+                    items.push(<li className="active"><a href="#">{i}</a></li>);
+                } else if (this.props.currentPage < i) {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/news"}>{i}</a>
+                    </li>);
+                } else {
+                    items.push(<li><a style={{marginLeft: 10 * {i} + 'px'}}
+                                      href={"/view/menu/" + this.props.menu[0]._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) - 1) + "/news"}>{i}</a>
+                    </li>);
 
                 }
-
-
-                prePage =(
+                prePage = (
                     <div className="fenye">
                         <div className="text-center">
                             <ul className="pagination">
@@ -200,20 +192,21 @@ export default class ListComponent  extends React.Component {
                 )
             }
         }
-        if(this.props.currentPage=='1'){
-            pageList=(
-                this.props.newsList.map((newsItem,index) =>
-                    <a key={index} className="collection-item listItem" key={index} href={"/view/menu/"+newsItem.menu_id._id+"/menuType/"+newsItem.menu_id.menu_type+"/page/"+(index+1)+"/newsListDetails"}>
+        if (this.props.currentPage == '1') {
+            pageList = (
+                this.props.newsList.map((newsItem, index) =>
+                    <a key={index} className="collection-item listItem" key={index}
+                       href={"/view/menu/" + newsItem.menu_id._id + "/menuType/" + newsItem.menu_id.menu_type + "/page/" + (index + 1) + "/newsListDetails"}>
                         <span className='fontWeight'> > </span>{newsItem.news_title}
                         <span className="rq">{newsItem.created_at.toLocaleDateString()}</span>
                     </a>
                 )
             )
-        }
-        else {
-            pageList=(
-                this.props.newsList.map((newsItem,index) =>
-                    <a key={index} className="collection-item listItem" key={index} href={"/view/menu/"+newsItem.menu_id._id+"/menuType/"+newsItem.menu_id.menu_type+"/page/"+((index+1)+(this.props.currentPage-1)*10)+"/newsListDetails"}>
+        } else {
+            pageList = (
+                this.props.newsList.map((newsItem, index) =>
+                    <a key={index} className="collection-item listItem" key={index}
+                       href={"/view/menu/" + newsItem.menu_id._id + "/menuType/" + newsItem.menu_id.menu_type + "/page/" + ((index + 1) + (this.props.currentPage - 1) * 10) + "/newsListDetails"}>
                         <span className='fontWeight'> > </span>{newsItem.news_title}
                         <span className="rq">{newsItem.created_at.toLocaleDateString()}</span>
                     </a>
@@ -226,14 +219,15 @@ export default class ListComponent  extends React.Component {
                 <Header menuList={this.props.menuList} logoTitle={this.props.logoTitle}/>
                 <div className="job-img margin-bottom-30" style={sectionStyle}></div>
                 <div className="container content profile wrap1003">
-                    <div className="row marginTop50" >
+                    <div className="row marginTop50">
                         <div className="col-md-3 md-margin-bottom-40">
                             <ul className="list-group sidebar-nav-v1 margin-bottom-40" id="sidebar-nav-1">
                                 <div className={"list-group-item menuName"}>{this.props.menu[0].menu_name}</div>
                                 {
-                                    this.props.twoMenuNameList.map((twoMenuNameItem,index) =>{
+                                    this.props.twoMenuNameList.map((twoMenuNameItem, index) => {
                                         return <li key={index} className={["list-group-item"]}>
-                                            <a  key={index} href={"/view/menu/"+twoMenuNameItem._id+"/menuType/"+twoMenuNameItem.menu_type+"/page/1/news"}>{twoMenuNameItem.menu_name}</a>
+                                            <a key={index}
+                                               href={"/view/menu/" + twoMenuNameItem._id + "/menuType/" + twoMenuNameItem.menu_type + "/page/1/news"}>{twoMenuNameItem.menu_name}</a>
                                         </li>
                                     })
                                 }
@@ -244,11 +238,12 @@ export default class ListComponent  extends React.Component {
                             <div className="kect">
                                 <div className="mores">
 
-                                    <a href={"/"} className="black-text" >首页</a>
+                                    <a href={"/"} className="black-text">首页</a>
                                     &nbsp;&gt;&nbsp;
-                                    <a className="black-text" href={"/view/menu/"+this.props.menu[0]._id+"/menuType/"+this.props.menu[0].menu_type+"/page/1/news"}>{this.props.menu[0].menu_name}</a>
+                                    <a className="black-text"
+                                       href={"/view/menu/" + this.props.menu[0]._id + "/menuType/" + this.props.menu[0].menu_type + "/page/1/news"}>{this.props.menu[0].menu_name}</a>
                                     {twoMenuName}
-                            </div>
+                                </div>
                             </div>
                             <div className="profile-body">
                                 <div className="row">
--- client/components/NewsDetailsComponent.jsx
+++ client/components/NewsDetailsComponent.jsx
@@ -2,30 +2,31 @@
 
 import React from 'react';
 
-import  Header from './layout/Header';
-import  Footer from './layout/Footer';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
 var sectionStyle;
 
-export default class NewsDetailsComponent  extends React.Component {
+export default class NewsDetailsComponent extends React.Component {
     constructor(props) {
         super(props);
     }
-    render () {
-        if(this.props.newsList[0].menu_id.banner_image&&this.props.newsList[0].menu_id.banner_image!==''&&this.props.newsList[0].menu_id.banner_image!==null){
-            sectionStyle={
-                backgroundImage: `url(`+(this.props.newsList[0].menu_id.banner_image)+`)`
+
+    render() {
+        if (this.props.newsList[0] && this.props.newsList[0].menu_id && this.props.newsList[0].menu_id.banner_image && this.props.newsList[0].menu_id.banner_image != '') {
+            sectionStyle = {
+                backgroundImage: `url(` + (this.props.newsList[0].menu_id.banner_image) + `)`
             };
-        }
-        else {
-            for(let i=0;i<this.props.menuList.length;i++) {
-                if(this.props.menuName==this.props.menuList[i].menu_name){
-                    if(this.props.menuList[i].banner_image==''||this.props.menuList[i].banner_image==null){
-                        sectionStyle={
+        } else {
+            for (let i = 0; i < this.props.menuList.length; i++) {
+                if (this.props.menuName == this.props.menuList[i].menu_name) {
+                    if (this.props.menuList[i].banner_image == '' || this.props.menuList[i].banner_image == null) {
+                        sectionStyle = {
                             backgroundImage: `url(/uploads/banner.png)`
                         };
-                    }else {
-                        sectionStyle={
-                            backgroundImage: `url(`+(this.props.menuList[i].banner_image)+`)`
+                    } else {
+                        sectionStyle = {
+                            backgroundImage: `url(` + (this.props.menuList[i].banner_image) + `)`
                         };
                     }
                 }
@@ -33,51 +34,47 @@ export default class NewsDetailsComponent  extends React.Component {
         }
 
 
-
         let prePage;
-        if(this.props.currentPage=='1'&&this.props.currentPage!==  ""+this.props.pageObj.totalCount){
-            prePage =(
+        if (this.props.currentPage == '1' && this.props.currentPage !== "" + this.props.pageObj.totalCount) {
+            prePage = (
                 <div className="fenPian">
-                    <ul role="menu" aria-label="Pagination" >
-                        <li>{<a role="menuitem" className='darkColor' style={{marginLeft:10+'px'}}>上一篇</a>}</li>
-                        <li>{<a role="menuitem"  className='greenColor' style={{marginLeft:10+'px'}}
-                                href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/newsListDetails"}>下一篇</a>}</li>
+                    <ul role="menu" aria-label="Pagination">
+                        <li>{<a role="menuitem" className='darkColor' style={{marginLeft: 10 + 'px'}}>上一篇</a>}</li>
+                        <li>{<a role="menuitem" className='greenColor' style={{marginLeft: 10 + 'px'}}
+                                href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/newsListDetails"}>下一篇</a>}</li>
                     </ul>
                 </div>
             )
 
-        }
-        else  if(this.props.currentPage!=='1'&&this.props.currentPage!==  ""+this.props.pageObj.totalCount){
-            prePage =(
+        } else if (this.props.currentPage !== '1' && this.props.currentPage !== "" + this.props.pageObj.totalCount) {
+            prePage = (
                 <div className="fenPian">
-                    <ul role="menu" aria-label="Pagination" >
-                        <li>{<a role="menuitem"   className='greenColor' style={{marginLeft:10+'px'}}
-                                href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/newsListDetails"}>上一篇</a>}</li>
-                        <li>{<a role="menuitem"  className='greenColor' style={{marginLeft:10+'px'}}
-                                href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)+1)+"/newsListDetails"}>下一篇</a>}</li>
+                    <ul role="menu" aria-label="Pagination">
+                        <li>{<a role="menuitem" className='greenColor' style={{marginLeft: 10 + 'px'}}
+                                href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) - 1) + "/newsListDetails"}>上一篇</a>}</li>
+                        <li>{<a role="menuitem" className='greenColor' style={{marginLeft: 10 + 'px'}}
+                                href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) + 1) + "/newsListDetails"}>下一篇</a>}</li>
                     </ul>
 
                 </div>
             )
-        }
-        else if(this.props.currentPage== ""+this.props.pageObj.totalCount&&this.props.currentPage!=='1'){
-            prePage =(
+        } else if (this.props.currentPage == "" + this.props.pageObj.totalCount && this.props.currentPage !== '1') {
+            prePage = (
                 <div className="fenPian">
-                    <ul role="menu" aria-label="Pagination" >
-                        <li>{<a role="menuitem"  className='greenColor' style={{marginLeft:10+'px'}}
-                                href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/2/page/"+(parseInt(this.props.currentPage)-1)+"/newsListDetails"}>上一篇</a>}</li>
-                        <li>{<a role="menuitem"  className='darkColor' style={{marginLeft:10+'px'}}>下一篇</a>}</li>
+                    <ul role="menu" aria-label="Pagination">
+                        <li>{<a role="menuitem" className='greenColor' style={{marginLeft: 10 + 'px'}}
+                                href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/2/page/" + (parseInt(this.props.currentPage) - 1) + "/newsListDetails"}>上一篇</a>}</li>
+                        <li>{<a role="menuitem" className='darkColor' style={{marginLeft: 10 + 'px'}}>下一篇</a>}</li>
                     </ul>
                 </div>
             )
-        }
-        else {
-            prePage =(
+        } else {
+            prePage = (
 
                 <div className="fenPian">
-                    <ul role="menu" aria-label="Pagination" >
-                        <li>{<a role="menuitem"  className='darkColor' style={{marginLeft:10+'px'}}>上一篇</a>}</li>
-                        <li>{<a role="menuitem"  className='darkColor' style={{marginLeft:10+'px'}}>下一篇</a>}</li>
+                    <ul role="menu" aria-label="Pagination">
+                        <li>{<a role="menuitem" className='darkColor' style={{marginLeft: 10 + 'px'}}>上一篇</a>}</li>
+                        <li>{<a role="menuitem" className='darkColor' style={{marginLeft: 10 + 'px'}}>下一篇</a>}</li>
                     </ul>
                 </div>
             )
@@ -86,9 +83,9 @@ export default class NewsDetailsComponent  extends React.Component {
 
             <div>
                 <Header menuList={this.props.menuList} logoTitle={this.props.logoTitle}/>
-                <div className="job-img margin-bottom-30 "  style={sectionStyle}></div>
+                <div className="job-img margin-bottom-30 " style={sectionStyle}></div>
                 <div className="container content profile wrap1003">
-                    <div className="row marginTop50" >
+                    <div className="row marginTop50">
                         <div className="col-md-3 md-margin-bottom-40">
 
                             <ul className="list-group sidebar-nav-v1 margin-bottom-40" id="sidebar-nav-1">
@@ -98,8 +95,10 @@ export default class NewsDetailsComponent  extends React.Component {
                         </div>
                         <div className="col-md-9">
                             <div className="kect">
-                                <div className="mores"><a href="/" className="black-text" >首页</a>&nbsp;&gt;&nbsp;
-                                    <a className="black-text"  href={"/view/menu/"+this.props.newsList[0].menu_id._id+"/menuType/"+this.props.newsList[0].menu_id.menu_type+"/page/1/news"}>{this.props.menuName}</a></div>
+                                <div className="mores"><a href="/" className="black-text">首页</a>&nbsp;&gt;&nbsp;
+                                    <a className="black-text"
+                                       href={"/view/menu/" + this.props.newsList[0].menu_id._id + "/menuType/" + this.props.newsList[0].menu_id.menu_type + "/page/1/news"}>{this.props.menuName}</a>
+                                </div>
                             </div>
 
                             <div className="profile-body">
@@ -108,10 +107,11 @@ export default class NewsDetailsComponent  extends React.Component {
 
                                         <div className="fenPianText">
                                             {
-                                                this.props.newsList.map((newsItem,index) =>
+                                                this.props.newsList.map((newsItem, index) =>
                                                     <div key={index}>
-                                                        <div className="titleh5"><h5 className="center-align">{newsItem.news_title}</h5></div>
-                                                        <p dangerouslySetInnerHTML={{ __html: newsItem.news_content }} />
+                                                        <div className="titleh5"><h5
+                                                            className="center-align">{newsItem.news_title}</h5></div>
+                                                        <p dangerouslySetInnerHTML={{__html: newsItem.news_content}}/>
 
                                                     </div>)
                                             }
